243c25b22cd928237b6218162d81595f5a04d926
Sudiptacoding <you@example.comsudiptabiswas506@gmail.com>
2023-12-03 16:42:37 +0600

    new peoject added

--- index.js
+++ index.js
@@ -81,10 +81,6 @@ async function run() {
             res.send(result)
         })
 
-
-
-        await client.connect();
-        await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
 
